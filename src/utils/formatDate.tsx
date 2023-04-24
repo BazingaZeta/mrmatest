cd971de4d8f3bd141cb18d565d52ab498104cfbf
Edoardo Trotta <edoardo@reiohr.com>
2023-04-24 22:26:36 +0100

    feat: add date formatter function

---
+++ src/utils/formatDate.tsx
@@ -0,0 +1,9 @@
+const formatDate = (dateString: string): string => {
+  const date = new Date(dateString);
+  const day = date.getDate();
+  const month = date.toLocaleString("default", { month: "short" });
+  const year = date.getFullYear();
+  return `${day} ${month} ${year}`;
+};
+
+export default formatDate;
