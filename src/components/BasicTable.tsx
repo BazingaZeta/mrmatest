b9e4d43a5a26f15f4a20d04af80d30aa9671e1e8
Edoardo Trotta <edoardo@reiohr.com>
2023-04-24 22:28:57 +0100

    feat: create BasicTable

---
+++ src/components/BasicTable.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { Table } from "@nextui-org/react";
+import { Composition } from "@/types/compositions/Composition";
+import formatDate from "../utils/formatDate";
+
+type BasicCardProps = {
+  columns: string[];
+  data: Composition[];
+};
+
+const BasicTable = ({ columns, data }: BasicCardProps) => {
+  return (
+    <Table
+      aria-label="Example static collection table with multiple selection"
+      css={{
+        height: "auto",
+        minWidth: "100%",
+      }}
+      selectionMode="multiple"
+    >
+      <Table.Header>
+        {columns.map((column: string, index: number) => (
+          <Table.Column key={index}>{column}</Table.Column>
+        ))}
+      </Table.Header>
+      <Table.Body>
+        {data.map((row: Composition, index: number) => (
+          <Table.Row key={index}>
+            <Table.Cell>{row.title}</Table.Cell>
+            <Table.Cell>{row.contributors.length}</Table.Cell>
+            <Table.Cell>{formatDate(row.created as string)}</Table.Cell>
+          </Table.Row>
+        ))}
+      </Table.Body>
+    </Table>
+  );
+};
+
+export default BasicTable;
